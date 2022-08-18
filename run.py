470adafe7c2ee925acde819fe187e70eee81b0d0
Nota <511947443>
2022-08-18 17:53:39 +0800

    update report save path

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import pytest
 import os
+import time
 
 
 if __name__ == '__main__':
     pytest.main(['-s', './Test_cases/test_case01.py', '--alluredir', './allure-results'])
-    os.system('allure generate ./allure-results -o ./Reports')
+    os.system('allure generate ./allure-results -o ./Output/Reports/report{0}'.format(time.strftime('%Y-%m-%d')))
